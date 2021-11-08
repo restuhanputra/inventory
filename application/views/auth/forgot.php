811177f3fa1d2d0e4d453f06401640d3a14558b6
restuhanputra <restu.hanputraa@gmail.com>
2021-11-09 03:42:51 +0700

    add: login view

--- application/views/auth/forgot.php
+++ application/views/auth/forgot.php
@@ -8,21 +8,21 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
 
-      <form action="#" method="POST">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Email" name="email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
+      <form action="" method="POST">
+        <!-- <div class="card-body"> -->
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="text" name="email" class="form-control <?= form_error('email') ? 'is-invalid' : ''; ?>" placeholder="Enter email" value="<?= set_value('email') ?>">
+          <span class="error invalid-feedback"><?= form_error('email') ?></span>
         </div>
-        <div class="row">
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">Request new password</button>
-          </div>
-          <!-- /.col -->
-        </div>
+        <!-- <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" name="password" class="form-control is-invalid" id="exampleInputPassword1" placeholder="Password" aria-describedby="exampleInputPassword1-error">
+            <span id="exampleInputPassword1-error" class="error invalid-feedback">Please provide a password</span>
+          </div> -->
+        <!-- </div> -->
+        <!-- /.card-body -->
+        <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 
       <p class="mt-3 mb-1">
--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -8,38 +8,18 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="#" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
+      <form action="" method="POST">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="text" name="email" class="form-control <?= form_error('email') ? 'is-invalid' : ''; ?>" placeholder="Enter email" value="<?= set_value('email') ?>">
+          <span class="error invalid-feedback"><?= form_error('email') ?></span>
         </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" name="password" class="form-control form-control <?= form_error('password') ? 'is-invalid' : ''; ?>" placeholder="Enter password">
+          <span class="error invalid-feedback"><?= form_error('password') ?></span>
         </div>
-        <div class="row">
-          <!-- <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div> -->
-          <!-- /.col -->
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 
       <!-- <div class="social-auth-links text-center mb-3">
