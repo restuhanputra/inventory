d5fff4f8b6f71119d2d365da9ec9cae2bf029c9e
restuhanputra <restu.hanputraa@gmail.com>
2021-11-14 16:02:01 +0700

    add: Admin_department controller

---
+++ application/controllers/Admin_department.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin_department extends CI_Controller
+{
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model("Admin_department_model", "Department");
+    $this->redirect = "department";
+  }
+
+  public function index()
+  {
+    $departmentData = $this->Department->getAllData();
+    $data = [
+      'title'          => 'Data Departemen',
+      'deskripsi'      => 'Melihat data departemen',
+      'departmentData' => $departmentData->result(),
+    ];
+    $page = 'department/index';
+    template($page, $data);
+  }
+
+  public function create()
+  {
+    $this->_validation();
+    if ($this->form_validation->run() == FALSE) {
+      $data = [
+        'title'     => 'Tambah Data Departemen',
+        'deskripsi' => 'Menambah data departemen',
+      ];
+      $page = 'department/create';
+      template($page, $data);
+    } else {
+      $this->db->set("id", "UUID()", FALSE);
+      $dataInsert = [
+        'department_name' => $this->input->post("name"),
+      ];
+      $insert = $this->Department->insert($dataInsert);
+      if ($insert > 0) {
+        $this->session->set_flashdata("success", "Data berhasil disimpan");
+      } else {
+        $this->session->set_flashdata("error", "Server sedang sibuk silahkan coba lagi");
+      }
+      redirect($this->redirect);
+    }
+  }
+
+  private function _validation($name = null)
+  {
+    $postName = $this->input->post("name");
+    if ($postName != $name) {
+      $is_unique = '|is_unique[m_department.department_name]';
+    } else {
+      $is_unique = '';
+    }
+
+    $this->form_validation->set_rules(
+      'name',
+      'Nama Departemen',
+      'trim|required' . $is_unique,
+      [
+        'required'  => '%s wajib diisi',
+        'is_unique' => '%s sudah ada',
+      ]
+    );
+  }
+}
