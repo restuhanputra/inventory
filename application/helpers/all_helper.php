1e9c12d315b1a2cd1ef7d64eff284c21ab9d8b71
restuhanputra <restu.hanputraa@gmail.com>
2021-11-20 16:09:12 +0700

    update: codeblock

--- application/helpers/all_helper.php
+++ application/helpers/all_helper.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @description load view header, topbar, sidebar, & footer
+ *
+ * @param string $page
+ * @param string $data
+ * @return void
+ */
 function template($page = null, $data = null)
 {
   $ci = get_instance();
@@ -10,6 +17,11 @@ function template($page = null, $data = null)
   $ci->load->view('admin/template/footer', $data);
 }
 
+/**
+ * @description Hapus session 'sucess' & 'error'
+ *
+ * @return void
+ */
 function usetFlash()
 {
   if (isset($_SESSION['success'])) {
